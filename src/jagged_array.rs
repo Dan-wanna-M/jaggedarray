b74241267f076183854ab420148aaab92c467e54
Huanghe <xs28@rice.edu>
2024-05-20 19:30:31 -0500

    - jagged array init

---
+++ src/jagged_array.rs
@@ -0,0 +1,429 @@
+use core::slice;
+use generic_array::{sequence::GenericSequence, ArrayLength, GenericArray};
+use std::{
+    iter::{once, zip},
+    ops::{Add, Index, IndexMut, Sub},
+    vec,
+};
+use typenum::{Const, IsEqual, NonZero, Sub1, Sum, ToUInt, Unsigned, Zero, B1, N1, U, U1, U2};
+
+use crate::utils::{self, Idx};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct JaggedArray<TVal, TNum, const N: usize>
+where
+    TNum: Idx<TNum>,
+    U<N>: std::ops::Sub<B1>,
+    <U<N> as std::ops::Sub<B1>>::Output: ArrayLength,
+    Const<N>: ToUInt,
+{
+    indices: GenericArray<Vec<TNum>, Sub1<U<N>>>,
+    buffer: Vec<TVal>,
+}
+
+pub struct Iter<'a, TVal> {
+    iter: slice::Iter<'a, TVal>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct JaggedArrayView<'a, TVal, TNum, const N: usize>
+where
+    TNum: Idx<TNum>,
+    U<N>: std::ops::Sub<B1>,
+    <U<N> as std::ops::Sub<B1>>::Output: ArrayLength,
+    Const<N>: ToUInt,
+{
+    indices: GenericArray<&'a [TNum], Sub1<U<N>>>,
+    buffer: &'a [TVal],
+}
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct JaggedArrayMutView<'a, TVal, TNum, const N: usize>
+where
+    TNum: Idx<TNum>,
+    U<N>: std::ops::Sub<B1>,
+    <U<N> as std::ops::Sub<B1>>::Output: ArrayLength,
+    Const<N>: ToUInt,
+{
+    indices: GenericArray<&'a mut [TNum], Sub1<U<N>>>,
+    buffer: &'a mut [TVal],
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct JaggedArrayOwnedView<TVal, TNum, const N: usize>
+where
+    TNum: Idx<TNum>,
+    U<N>: std::ops::Sub<B1>,
+    <U<N> as std::ops::Sub<B1>>::Output: ArrayLength,
+    Const<N>: ToUInt,
+{
+    indices: GenericArray<Box<[TNum]>, Sub1<U<N>>>,
+    buffer: Box<[TVal]>,
+}
+
+impl<TVal, TNum, const N: usize> Default for JaggedArray<TVal, TNum, N>
+where
+    TNum: Idx<TNum>,
+    U<N>: std::ops::Sub<B1>,
+    <U<N> as std::ops::Sub<B1>>::Output: ArrayLength,
+    Const<N>: ToUInt,
+{
+    #[inline]
+    fn default() -> Self {
+        Self {
+            indices: GenericArray::generate(|_| vec![0.into()]),
+            buffer: Default::default(),
+        }
+    }
+}
+// Methods that are unique to JaggedArray
+impl<TVal, TNum, const N: usize> JaggedArray<TVal, TNum, N>
+where
+    TNum: Idx<TNum>,
+    U<N>: std::ops::Sub<B1>,
+    <U<N> as std::ops::Sub<B1>>::Output: ArrayLength,
+    Const<N>: ToUInt,
+{
+    #[inline]
+    pub fn new() -> Self {
+        Self::default()
+    }
+    #[inline]
+    pub fn with_capacity(capacity: [usize; N]) -> Self {
+        Self {
+            indices: GenericArray::generate(|i| {
+                let mut temp = vec![0.into()];
+                temp.reserve(capacity[i]);
+                temp
+            }),
+            buffer: Vec::with_capacity(*capacity.last().unwrap()),
+        }
+    }
+    pub fn reserve(&mut self, additional: [usize; N]) {
+        for (index, additional) in zip(self.indices.iter_mut(), additional.iter()) {
+            index.reserve(*additional);
+        }
+        self.buffer.reserve(additional[N - 1]);
+    }
+
+    #[inline]
+    pub fn clear(&mut self) {
+        self.buffer.clear();
+        for index in self.indices.iter_mut() {
+            index.clear();
+            index.push(0.into());
+        }
+    }
+    pub fn new_row<const DIM: usize>(&mut self)
+    where
+        U<N>: std::ops::Sub<U<DIM>>,
+        Sub1<<U<N> as std::ops::Sub<U<DIM>>>::Output>: Unsigned + NonZero,
+        <U<N> as std::ops::Sub<U<DIM>>>::Output: std::ops::Sub<typenum::B1>,
+        U<DIM>: ArrayLength,
+        Const<N>: ToUInt,
+        Const<DIM>: ToUInt,
+    {
+        let m = DIM;
+        let buffer = &mut self.indices[m];
+        let new_val = *buffer.last().unwrap();
+        buffer.push(new_val);
+        if m > 0 {
+            *self.indices[m - 1].last_mut().unwrap() += 1.into();
+        }
+    }
+    #[inline]
+    pub fn push_to_last_row(&mut self, val: TVal) {
+        self.buffer.push(val);
+        *self.indices.last_mut().unwrap().last_mut().unwrap() += 1.into();
+    }
+    #[inline]
+    pub fn pop_from_last_row(&mut self) -> Option<TVal> {
+        let mut iter = self.indices.last_mut().unwrap().iter_mut().rev();
+        let last = iter.next().unwrap();
+        if *last != 0.into() && iter.next().unwrap() > last {
+            *last -= 1.into();
+            self.buffer.pop()
+        } else {
+            None
+        }
+    }
+    pub fn extend_last_row(&mut self, values: impl Iterator<Item = TVal>) {
+        let initial = self.buffer.len();
+        self.buffer.extend(values);
+        *self.indices.last_mut().unwrap().last_mut().unwrap() +=
+            (self.buffer.len() - initial).into();
+    }
+    // It may be possible to implement a drain-like variant of this method
+    pub fn remove_last_row<const DIM: usize>(&mut self) -> Option<()>
+    where
+        U<N>: std::ops::Sub<U<DIM>>,
+        Sub1<<U<N> as std::ops::Sub<U<DIM>>>::Output>: Unsigned + NonZero,
+        <U<N> as std::ops::Sub<U<DIM>>>::Output: std::ops::Sub<typenum::B1>,
+        U<DIM>: ArrayLength,
+        Const<N>: ToUInt,
+        Const<DIM>: ToUInt,
+    {
+        let mut iter = self.indices[DIM].iter().rev();
+        let last = *iter.next().unwrap();
+        if last != 0.into() {
+            let mut last = *iter.next().unwrap();
+            for index in self.indices.iter_mut().skip(DIM + 1) {
+                index.truncate(usize::max(1, last.into()));
+                last = *index.last().unwrap();
+            }
+            self.buffer.truncate(last.into());
+            self.indices[DIM].pop();
+            Some(())
+        } else {
+            None
+        }
+    }
+}
+
+pub trait JaggedArrayViewTrait<TVal, TNum, const N: usize>: Index<[usize; N]>
+where
+    TNum: Idx<TNum>,
+    U<N>: std::ops::Sub<U2> + std::ops::Sub<typenum::B1> + ArrayLength,
+    <U<N> as std::ops::Sub<typenum::B1>>::Output: ArrayLength,
+    <U<N> as std::ops::Sub<U2>>::Output: ArrayLength,
+    <U<N> as std::ops::Sub<typenum::B1>>::Output: std::ops::Sub<typenum::B1>,
+    <<U<N> as std::ops::Sub<typenum::B1>>::Output as std::ops::Sub<typenum::B1>>::Output:
+        ArrayLength,
+    Const<N>: ToUInt,
+{
+    fn is_empty(&self) -> bool;
+    fn dims(&self) -> [usize; N];
+    fn view<const M: usize, const R: usize>(
+        &self,
+        index: [usize; M],
+    ) -> JaggedArrayView<TVal, TNum, R>
+    where
+        U<N>: std::ops::Sub<U<M>>,
+        <U<N> as std::ops::Sub<U<M>>>::Output: IsEqual<U<R>>,
+        U<R>: std::ops::Sub<B1>,
+        <U<R> as std::ops::Sub<B1>>::Output: ArrayLength,
+        <<U<N> as std::ops::Sub<U<M>>>::Output as IsEqual<U<R>>>::Output: NonZero,
+        Const<N>: ToUInt,
+        Const<M>: ToUInt,
+        Const<R>: ToUInt;
+    fn to_owned(self) -> JaggedArrayOwnedView<TVal, TNum, N>
+    where
+        TVal: Clone;
+}
+
+pub trait JaggedArrayMutViewTrait<TVal, TNum, const N: usize>:
+    JaggedArrayViewTrait<TVal, TNum, N> + IndexMut<[usize; N]>
+where
+    TNum: Idx<TNum>,
+    U<N>: std::ops::Sub<U2> + std::ops::Sub<typenum::B1> + ArrayLength,
+    <U<N> as std::ops::Sub<typenum::B1>>::Output: ArrayLength,
+    <U<N> as std::ops::Sub<U2>>::Output: ArrayLength,
+    <U<N> as std::ops::Sub<typenum::B1>>::Output: std::ops::Sub<typenum::B1>,
+    <<U<N> as std::ops::Sub<typenum::B1>>::Output as std::ops::Sub<typenum::B1>>::Output:
+        ArrayLength,
+    Const<N>: ToUInt,
+{
+    fn view_mut<const M: usize, const R: usize>(
+        &mut self,
+        index: [usize; M],
+    ) -> JaggedArrayMutView<TVal, TNum, R>
+    where
+        U<N>: std::ops::Sub<U<M>>,
+        <U<N> as std::ops::Sub<U<M>>>::Output: IsEqual<U<R>>,
+        U<R>: std::ops::Sub<B1>,
+        <U<R> as std::ops::Sub<B1>>::Output: ArrayLength,
+        <<U<N> as std::ops::Sub<U<M>>>::Output as IsEqual<U<R>>>::Output: NonZero,
+        Const<N>: ToUInt,
+        Const<M>: ToUInt,
+        Const<R>: ToUInt;
+}
+macro_rules! impl_view {
+    ( $typ:ident < $( $gen:tt ),+ > ) => {
+        impl<$( $gen ),+,const N:usize> JaggedArrayViewTrait<TVal, TNum, N>
+            for $typ<$($gen),+, N>
+        where
+            TNum: Idx<TNum>,
+            U<N>: std::ops::Sub<U2> + std::ops::Sub<typenum::B1> + ArrayLength,
+            <U<N> as std::ops::Sub<typenum::B1>>::Output: ArrayLength,
+            <U<N> as std::ops::Sub<U2>>::Output: ArrayLength,
+            <U<N> as std::ops::Sub<typenum::B1>>::Output: std::ops::Sub<typenum::B1>,
+            <<U<N> as std::ops::Sub<typenum::B1>>::Output as std::ops::Sub<typenum::B1>>::Output:
+                ArrayLength,
+            Const<N>: ToUInt,
+        {
+            #[inline]
+            fn dims(&self) -> [usize; N] {
+                let mut result = [0usize; N];
+                for (dst, src) in zip(result.iter_mut(), self.indices.iter()) {
+                    *dst = src.len();
+                }
+                result[N - 1] = self.buffer.len();
+                result
+            }
+            #[inline]
+            fn is_empty(&self) -> bool {
+                self.buffer.is_empty()
+            }
+            /// Rust const generics does not support arithmetic, so we have to specify the view's dimension(R) as well
+            fn view<const M: usize, const R: usize>(
+                &self,
+                index: [usize; M],
+            ) -> JaggedArrayView<TVal, TNum, R>
+            where
+                U<N>: std::ops::Sub<U<M>>,
+                <U<N> as std::ops::Sub<U<M>>>::Output: IsEqual<U<R>>,
+                U<R>: std::ops::Sub<B1>,
+                <U<R> as std::ops::Sub<B1>>::Output: ArrayLength,
+                <<U<N> as std::ops::Sub<U<M>>>::Output as IsEqual<U<R>>>::Output: NonZero,
+                Const<N>: ToUInt,
+                Const<M>: ToUInt,
+                Const<R>: ToUInt,
+            {
+                let (first,remaining) = self.indices.split_at(M);
+                let (index_buffer, self_indices) = first.split_first().unwrap();
+                let mut index_buffer = &index_buffer[..];
+                for (&i, idx) in zip(index.iter(), self_indices.iter()) {
+                    index_buffer = &idx[index_buffer[i].into()..index_buffer[i + 1].into() + 1]
+                }
+                let mut result = GenericArray::<&[TNum], Sub1<U<R>>>::uninit();
+                let (indices, buffer) = if R > 1 {
+                    result[0].write(index_buffer);
+                    for (src,dst) in remaining.iter().zip(result.iter_mut().skip(1)) {
+                        dst.write(src);
+                    }
+                    // SAFETY: Now safe as we initialized all elements from 0 to R-1
+                    (
+                        unsafe { GenericArray::assume_init(result) },
+                        &self.buffer[..],
+                    )
+                } else {
+                    let start_index = index_buffer[*index.last().unwrap()].into();
+                    let end_index = index_buffer[*index.last().unwrap() + 1].into();
+                    // SAFETY: zero-sized arrays don't need initialization
+                    (
+                        unsafe { GenericArray::assume_init(result) },
+                        &self.buffer[start_index..end_index],
+                    )
+                };
+                JaggedArrayView { indices, buffer }
+            }
+            fn to_owned(self) -> JaggedArrayOwnedView<TVal, TNum, N> where TVal:Clone {
+                let indices = self.indices.iter().map(|idx| idx.to_vec().into_boxed_slice()).collect();
+                let buffer = self.buffer.to_vec().into_boxed_slice();
+                JaggedArrayOwnedView { indices, buffer }
+            }
+        }
+
+        impl<$( $gen ),+,const N:usize> Index<[usize; N]> for $typ<$($gen),+, N>
+        where
+            TNum: Idx<TNum>,
+            U<N>: std::ops::Sub<B1> + ArrayLength,
+            <U<N> as std::ops::Sub<B1>>::Output: ArrayLength,
+            Const<N>: ToUInt,
+        {
+            type Output = TVal;
+            fn index(&self, index: [usize; N]) -> &Self::Output {
+                if N > 1 {
+                    let mut buffer = &self.indices[0][..];
+                    for (&i, idx) in zip(index.iter(), self.indices[1..].iter()) {
+                        buffer = &idx[buffer[i].into()..buffer[i + 1].into() + 1]
+                    }
+                    let start_index = buffer[index[index.len() - 2]].into();
+                    let end_index = buffer[index[index.len() - 2] + 1].into();
+                    &self.buffer[start_index..end_index][index[index.len() - 1]]
+                } else {
+                    &self.buffer[index[0]]
+                }
+            }
+        }
+    };
+}
+
+macro_rules! impl_view_mut {
+    ( $typ:ident < $( $gen:tt ),+ > ) => {
+        impl<$( $gen ),+,const N:usize> JaggedArrayMutViewTrait<TVal, TNum, N>
+            for $typ<$($gen),+, N>
+        where
+            TNum: Idx<TNum>,
+            U<N>: std::ops::Sub<U2> + std::ops::Sub<typenum::B1> + ArrayLength,
+            <U<N> as std::ops::Sub<typenum::B1>>::Output: ArrayLength,
+            <U<N> as std::ops::Sub<U2>>::Output: ArrayLength,
+            <U<N> as std::ops::Sub<typenum::B1>>::Output: std::ops::Sub<typenum::B1>,
+            <<U<N> as std::ops::Sub<typenum::B1>>::Output as std::ops::Sub<typenum::B1>>::Output:
+                ArrayLength,
+            Const<N>: ToUInt,
+        {
+            /// Rust const generics does not support arithmetic, so we have to specify the view's dimension(R) as well
+            fn view_mut<const M: usize, const R: usize>(
+                &mut self,
+                index: [usize; M],
+            ) -> JaggedArrayMutView<TVal, TNum, R>
+            where
+                U<N>: std::ops::Sub<U<M>>,
+                <U<N> as std::ops::Sub<U<M>>>::Output: IsEqual<U<R>>,
+                U<R>: std::ops::Sub<B1>,
+                <U<R> as std::ops::Sub<B1>>::Output: ArrayLength,
+                <<U<N> as std::ops::Sub<U<M>>>::Output as IsEqual<U<R>>>::Output: NonZero,
+                Const<N>: ToUInt,
+                Const<M>: ToUInt,
+                Const<R>: ToUInt,
+            {
+                let (first,remaining) = self.indices.split_at_mut(M);
+                let (index_buffer, self_indices) = first.split_first_mut().unwrap();
+                let mut index_buffer = &mut index_buffer[..];
+                for (&i, idx) in zip(index.iter(), self_indices.iter_mut()) {
+                    index_buffer = &mut idx[index_buffer[i].into()..index_buffer[i + 1].into() + 1]
+                }
+                let mut result = GenericArray::<&mut [TNum], Sub1<U<R>>>::uninit();
+                let (indices, buffer) = if R > 1 {
+                    result[0].write(index_buffer);
+                    for (src,dst) in remaining.iter_mut().zip(result.iter_mut().skip(1)) {
+                        dst.write(src);
+                    }
+                    // SAFETY: Now safe as we initialized all elements from 0 to R-1
+                    (
+                        unsafe { GenericArray::assume_init(result) },
+                        &mut self.buffer[..],
+                    )
+                } else {
+                    let start_index = index_buffer[*index.last().unwrap()].into();
+                    let end_index = index_buffer[*index.last().unwrap() + 1].into();
+                    // SAFETY: zero-sized arrays don't need initialization
+                    (
+                        unsafe { GenericArray::assume_init(result) },
+                        &mut self.buffer[start_index..end_index],
+                    )
+                };
+                JaggedArrayMutView { indices, buffer }
+            }
+        }
+
+        impl<$( $gen ),+,const N:usize> IndexMut<[usize; N]> for $typ<$($gen),+, N>
+        where
+            TNum: Idx<TNum>,
+            U<N>: std::ops::Sub<B1> + ArrayLength,
+            <U<N> as std::ops::Sub<B1>>::Output: ArrayLength,
+            Const<N>: ToUInt,
+        {
+            fn index_mut(&mut self, index: [usize; N]) -> &mut TVal {
+                if N > 1 {
+                    let mut buffer = &self.indices[0][..];
+                    for (&i, idx) in zip(index.iter(), self.indices[1..].iter()) {
+                        buffer = &idx[buffer[i].into()..buffer[i + 1].into() + 1]
+                    }
+                    let start_index = buffer[index[index.len() - 2]].into();
+                    let end_index = buffer[index[index.len() - 2] + 1].into();
+                    &mut self.buffer[start_index..end_index][index[index.len() - 1]]
+                } else {
+                    &mut self.buffer[index[0]]
+                }
+            }
+        }
+    };
+}
+impl_view!(JaggedArray<TVal, TNum>);
+impl_view!(JaggedArrayView<'a, TVal, TNum>);
+impl_view!(JaggedArrayMutView<'a, TVal, TNum>);
+impl_view!(JaggedArrayOwnedView<TVal, TNum>);
+impl_view_mut!(JaggedArray<TVal, TNum>);
+impl_view_mut!(JaggedArrayMutView<'a, TVal, TNum>);
+impl_view_mut!(JaggedArrayOwnedView<TVal, TNum>);
---
+++ src/lib.rs
@@ -0,0 +1,157 @@
+pub mod jagged_array;
+#[macro_use]
+pub mod utils;
+#[cfg(test)]
+mod tests {
+    use generic_array::arr;
+    use typenum::{N1, U0, U1, U2};
+
+    use crate::{jagged_array::JaggedArrayViewTrait, utils::U16};
+
+    use super::*;
+    #[test]
+    fn push_1d_test() {
+        let mut data = jagged_array::JaggedArray::<i32, U16, 1>::new();
+        // data.new_row::<0>();
+        // data.view();
+        data.push_to_last_row(1);
+        assert!(data[[0]] == 1);
+        data.push_to_last_row(4);
+        data.push_to_last_row(5);
+        data.push_to_last_row(6);
+        assert!(data[[1]] == 4);
+        assert!(data[[2]] == 5);
+        assert!(data[[3]] == 6);
+        data.push_to_last_row(7);
+        assert!(data[[4]] == 7);
+    }
+
+    #[test]
+    fn push_2d_test() {
+        let mut data = jagged_array::JaggedArray::<i32, U16, 2>::new();
+        data.new_row::<0>();
+        data.push_to_last_row(1);
+        assert!(data[[0,0]] == 1);
+        assert!(data.view::<1,1>([0])[[0]]==1);
+        data.new_row::<0>();
+        data.push_to_last_row(4);
+        data.push_to_last_row(5);
+        data.push_to_last_row(6);
+        assert!(data[[1,0]] == 4);
+        assert!(data.view::<1,1>([1])[[0]]==4);
+        assert!(data[[1,1]] == 5);
+        assert!(data.view::<1,1>([1])[[1]]==5);
+        assert!(data[[1,2]] == 6);
+        assert!(data.view::<1,1>([1])[[2]]==6);
+        data.new_row::<0>();
+        data.new_row::<0>();
+        data.push_to_last_row(7);
+        assert!(data[[3,0]] == 7);
+        assert!(data.view::<1,1>([3])[[0]]==7);
+        data.remove_last_row::<0>();
+        assert!(data[[0,0]] == 1);
+        data.remove_last_row::<0>();
+        data.remove_last_row::<0>();
+        data.remove_last_row::<0>();
+        assert!(data.is_empty());
+    }
+
+    #[test]
+    fn push_3d_test() {
+        let mut data = jagged_array::JaggedArray::<i32, U16, 3>::new();
+        data.new_row::<0>();
+        data.new_row::<1>();
+        data.push_to_last_row(1);
+        assert!(data[[0,0,0]] == 1);
+        assert!(data.view::<1,2>([0]).view::<1,1>([0])[[0]]==1);
+        data.new_row::<1>();
+        data.push_to_last_row(4);
+        data.push_to_last_row(5);
+        assert!(data[[0, 1, 0]] == 4);
+        assert!(data[[0, 1, 1]] == 5);
+        data.new_row::<1>();
+        data.push_to_last_row(7);
+        data.push_to_last_row(8);
+        data.push_to_last_row(9);
+        assert!(data[[0,2,0]] == 7);
+        assert!(data[[0,2,1]] == 8);
+        assert!(data[[0,2,2]] == 9);
+        data.new_row::<0>();
+        data.new_row::<1>();
+        data.push_to_last_row(10);
+        data.push_to_last_row(11);
+        data.push_to_last_row(12);
+        data.push_to_last_row(13);
+        assert!(data[[1,0,0]] == 10);
+        assert!(data[[1,0,1]] == 11);
+        assert!(data[[1,0,2]] == 12);
+        assert!(data[[1,0,3]] == 13);
+        data.new_row::<0>();
+        data.new_row::<0>();
+        data.new_row::<1>();
+        data.push_to_last_row(100);
+
+        // assert!(data[[2, 0, 0]] == 100);
+        assert!(data[[3, 0, 0]] == 100);
+        data.remove_last_row::<0>();
+        assert!(data[[0, 0, 0]] == 1);
+        assert!(data[[0, 1, 0]] == 4);
+        assert!(data[[0, 1, 1]] == 5);
+        assert!(data[[0, 2, 0]] == 7);
+        assert!(data[[0, 2, 1]] == 8);
+        assert!(data[[0, 2, 2]] == 9);
+    }
+    #[test]
+    fn push_4d_test() {
+        let mut data = jagged_array::JaggedArray::<i32, U16, 4>::new();
+        data.new_row::<0>();
+        data.new_row::<1>();
+        data.new_row::<2>();
+        data.push_to_last_row(1);
+        data.new_row::<2>();
+        data.push_to_last_row(4);
+        data.push_to_last_row(5);
+        data.new_row::<2>();
+        data.push_to_last_row(7);
+        data.push_to_last_row(8);
+        data.push_to_last_row(9);
+        data.new_row::<1>();
+        data.new_row::<2>();
+        data.push_to_last_row(10);
+        data.push_to_last_row(11);
+        data.push_to_last_row(12);
+        data.push_to_last_row(13);
+        data.new_row::<1>();
+        data.new_row::<1>();
+        data.new_row::<2>();
+        data.push_to_last_row(100);
+        assert!(data[[0, 0, 0, 0]] == 1);
+        assert!(data[[0, 0, 1, 0]] == 4);
+        assert!(data[[0, 0, 1, 1]] == 5);
+        assert!(data[[0, 0, 2, 0]] == 7);
+        assert!(data[[0, 0, 2, 1]] == 8);
+        assert!(data[[0, 0, 2, 2]] == 9);
+        assert!(data[[0, 1, 0, 0]] == 10);
+        assert!(data[[0, 1, 0, 1]] == 11);
+        assert!(data[[0, 1, 0, 2]] == 12);
+        assert!(data[[0, 1, 0, 3]] == 13);
+        assert!(data[[0, 3, 0, 0]] == 100);
+        data.new_row::<0>();
+        data.new_row::<0>();
+        data.new_row::<1>();
+        data.new_row::<2>();
+        data.new_row::<2>();
+        data.new_row::<0>();
+        data.new_row::<1>();
+        data.new_row::<2>();
+        data.new_row::<2>();
+        data.push_to_last_row(1000);
+        data.push_to_last_row(1100);
+        data.push_to_last_row(1200);
+        data.push_to_last_row(1300);
+        assert!(data[[3, 0, 1, 0]] == 1000);
+        assert!(data[[3, 0, 1, 1]] == 1100);
+        assert!(data[[3, 0, 1, 2]] == 1200);
+        assert!(data[[3, 0, 1, 3]] == 1300);
+    }
+}
